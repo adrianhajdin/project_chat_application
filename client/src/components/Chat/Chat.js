29317bc17cf7d7fb089ba06d6b7a458ab5bab4b5
Vighnesh Deep Sharan <vighneshdeepsharan2002@gmail.com>
2020-10-03 20:42:05 +0530

    Change from my side

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+  const ENDPOINT = 'https://application-real-chat.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,7 +10,7 @@ export default function SignIn() {
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
+        <h1 className="heading">Welcome!!!</h1>
         <div>
           <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
@@ -18,7 +18,7 @@ export default function SignIn() {
           <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className={'button mt-20'} type="submit">Sign In</button>
+          <button className={'button mt-20'} type="submit">Join</button>
         </Link>
       </div>
     </div>
