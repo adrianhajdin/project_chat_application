5812f931484ebb863c228a8471770e7f7721e2a5
Michael Carr <xyeres@users.noreply.github.com>
2021-08-27 12:54:37 -0700

    Fixes removeUser splice
    Fixes incorrect splice end param

--- server/users.js
+++ server/users.js
@@ -19,7 +19,7 @@ const addUser = ({ id, name, room }) => {
 const removeUser = (id) => {
   const index = users.findIndex((user) => user.id === id);
 
-  if(index !== -1) return users.splice(index, 1)[0];
+  if(index !== -1) return users.splice(index, index+1)[0];
 }
 
 const getUser = (id) => users.find((user) => user.id === id);
