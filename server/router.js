b420fecf3c4f677be6666ff19cae2d687f9ac7e6
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-03 16:34:24 +0800

    Delete server directory

--- server/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
-});
-
-module.exports = router;
