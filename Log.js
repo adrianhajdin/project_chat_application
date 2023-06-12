64d0fe10c5b92f3cbe16f45c4f5d563520c82b5d
Muhammed Shahinsha .c <122088649+ShahinshaShahinu@users.noreply.github.com>
2023-06-12 07:55:49 +0530

    Next another Update Log.js

--- Log.js
+++ Log.js
@@ -1,7 +1,4 @@
 console.log('hello world);
             console.log('hello world);
             console.log('hello world);
-            console.log('hello world);
-            console.log('hello world);
-            console.log('hello world);
             
