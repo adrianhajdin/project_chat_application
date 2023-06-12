b91f29a2e7adcf8358978f8cf21c0b7c9893b7ae
Muhammed Shahinsha .c <122088649+ShahinshaShahinu@users.noreply.github.com>
2023-06-12 07:54:57 +0530

    Create Log.js

---
+++ Log.js
@@ -0,0 +1,7 @@
+console.log('hello world);
+            console.log('hello world);
+            console.log('hello world);
+            console.log('hello world);
+            console.log('hello world);
+            console.log('hello world);
+            
