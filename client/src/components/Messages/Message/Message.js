062e9576be829cd5c610a69e6381dcdbe7e74a51
prathamsuroshi <psuroshi9@gmail.com>
2023-09-26 20:14:23 +0530

    fixed TypeError

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -7,7 +7,7 @@ import ReactEmoji from 'react-emoji';
 const Message = ({ message: { text, user }, name }) => {
   let isSentByCurrentUser = false;
 
-  const trimmedName = name.trim().toLowerCase();
+  const trimmedName = name ? name.trim().toLowerCase() : '';
 
   if(user === trimmedName) {
     isSentByCurrentUser = true;
