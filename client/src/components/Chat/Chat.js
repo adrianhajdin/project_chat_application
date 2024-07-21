b17937b92dbe9d8c9c6499c5c3d64d7f6b91302a
Martin <mmaina290@gmail.com>
2024-07-21 03:18:51 +0300

    fixing errors on chat.js and TextContainer.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
-
 import TextContainer from '../TextContainer/TextContainer';
 import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
+import Proptype from 'prop-types';
 
 import './Chat.css';
 
@@ -14,19 +14,19 @@ const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 let socket;
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+  const [setName] = useState('');
+  const [setRoom] = useState('');
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    const { name, room } = queryString.parse(location.search);
+    const { room, name } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
     setRoom(room);
-    setName(name)
+    setName(name);
 
     socket.emit('join', { name, room }, (error) => {
       if(error) {
@@ -63,6 +63,10 @@ const Chat = ({ location }) => {
       <TextContainer users={users}/>
     </div>
   );
+}
+
+Chat.propTypes = {
+  location: Proptype.object
 }
 
 export default Chat;
--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,9 +1,6 @@
 import React from 'react';
-
 import ScrollToBottom from 'react-scroll-to-bottom';
-
 import Message from './Message/Message';
-
 import './Messages.css';
 
 const Messages = ({ messages, name }) => (
