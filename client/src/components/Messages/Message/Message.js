cb5f31ea48ddea7750d3006e0dd496a09bff6ae8
WNandini <23nandiniwadekar@gmail.com>
2024-08-10 17:58:22 +0530

    User name capital

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -4,9 +4,14 @@ import './Message.css';
 
 import ReactEmoji from 'react-emoji';
 
+const capitalizeName = (name) => {
+  let data = name
+  let char = data.slice(0,1).toUpperCase()
+  return name.replace(data.charAt(0),char)
+}
+
 const Message = ({ message: { text, user }, name }) => {
   let isSentByCurrentUser = false;
-
   const trimmedName = name.trim().toLowerCase();
 
   if(user === trimmedName) {
@@ -17,7 +22,7 @@ const Message = ({ message: { text, user }, name }) => {
     isSentByCurrentUser
       ? (
         <div className="messageContainer justifyEnd">
-          <p className="sentText pr-10">{trimmedName}</p>
+          <p className="sentText pr-10">{capitalizeName(trimmedName)}</p>
           <div className="messageBox backgroundBlue">
             <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
           </div>
@@ -28,7 +33,7 @@ const Message = ({ message: { text, user }, name }) => {
             <div className="messageBox backgroundLight">
               <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
             </div>
-            <p className="sentText pl-10 ">{user}</p>
+            <p className="sentText pl-10 ">{capitalizeName(user)}</p>
           </div>
         )
   );
