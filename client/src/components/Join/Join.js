82386a0b0d3743029f9d035f8e0d05c4293c38d2
WNandini <23nandiniwadekar@gmail.com>
2024-08-10 18:43:21 +0530

    User Experience Improved

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -18,7 +18,7 @@ export default function SignIn() {
           <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className={'button mt-20'} type="submit">Sign In</button>
+          <button className={'joinBtnWidth blueBtn mt-20'} type="submit">Sign In</button>
         </Link>
       </div>
     </div>
